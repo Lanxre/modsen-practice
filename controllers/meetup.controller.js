b728490f08f1266c45956ad781f766b44e571577
Vittalius <Vittalius@users.noreply.github.com>
2023-03-22 19:17:07 +0300

    update controller

--- controllers/meetup.controller.js
+++ controllers/meetup.controller.js
@@ -1,53 +1,38 @@
-import db from "../database/db.js"
+import MeetUpDTO from "../dtos/meetup.dto.js"
+import MeetUpService from "../services/meetup.service.js"
 
 export default class MeetUpController{
-    async createMeet(req, res){
-        const {
-            theme_meet,
-            description_meet,
-            tags,
-            locate_meet
-        } = req.body
-        
-        const meetUp = await db.query(
-            `INSERT INTO meetup (theme_meet, description_meet, tags, locate_meet) ` +
-            `values ($1, $2, $3, $4) RETURNING *`,
-            [theme_meet, description_meet, tags, locate_meet]
-        )
-        res.json(await meetUp.rows[0])
+
+    constructor(){
+        this.service = new MeetUpService() 
     }
-    async getMeet(req, res){
-        const meetUps = await db.query(`SELECT * FROM meetup`)
-        res.json(meetUps.rows)
+
+    async createMeet(req, res){
+        const meetUpDto = new MeetUpDTO(req.body)
+        const meetUp = await this.service.createMeetUp(meetUpDto)
+        
+        res.json(meetUp)
+    }
+    async getMeets(req, res){
+        const meetUps = await this.service.getMeetUps()
+        
+        res.json(meetUps)
     }
     async getOneMeet(req, res){
-        const id = req.params.id
-        const meetUps = await db.query(`SELECT * FROM meetup where id = $1`, [id])
-        res.json(meetUps.rows[0])
+        const meetUp = await this.service.getOneMeetUp(req.params.id)
+
+        res.json(meetUp)
     }
     async updateMeet(req, res){
-        const id = req.params.id
-        const {
-            theme_meet,
-            description_meet,
-            tags,
-            locate_meet
-        } = req.body
+        const meetUpDto = new MeetUpDTO({...req.body, id: req.params.id})
+        const meetUpResult = await this.service.updateMeetUp(meetUpDto)
 
-        const meetUp = await db.query(
-            'UPDATE meetups set theme_meet = $1, description_meet = $2, tags = $3, locate_meet = $4 ' +
-            'where id = $5 RETURNING *',
-            [theme_meet, description_meet, tags, locate_meet, id]
-        )
-        res.json(meetUp.rows[0])
+        res.json(meetUpResult)
     }
     async deleteMeet(req, res){
-        const id = req.params.id
-        const meetUp = await db.query(
-            'DELETE FROM meetup where id = $1',
-            [id]
-        )
-        res.json(meetUp.rows[0])
+        const meetUp = await this.service.deleteMeetUp(req.params.id)
+        
+        res.json(meetUp)
     }
 
 }
