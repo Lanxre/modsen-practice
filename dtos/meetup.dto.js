5b638893f2ac3709589a178121119ed2678a5f17
Vittalius <Vittalius@users.noreply.github.com>
2023-03-22 19:16:37 +0300

    create dto

---
+++ dtos/meetup.dto.js
@@ -0,0 +1,15 @@
+export default class MeetUpDTO{
+
+    constructor({
+        id,
+        theme_meet,
+        tags,
+        locate_meet
+    }){
+        this.id = id;
+        this.theme_meet = theme_meet;
+        this.tags = tags;
+        this.locate_meet = locate_meet;
+    }
+
+}
