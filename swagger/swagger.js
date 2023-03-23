ce2397340f85f8a2a9a04df8b55048b2d4b2bd03
Vittalius <Vittalius@users.noreply.github.com>
2023-03-23 21:13:48 +0300

    swagger
    add docs

---
+++ swagger/swagger.js
@@ -0,0 +1,52 @@
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+import swaggerAutogen from 'swagger-autogen'
+
+const _dirname = dirname(fileURLToPath(import.meta.url))
+
+const doc = {
+    info: {
+      title: 'MeetUps API',
+      description: 'MeetUps API for practice'
+    },
+
+    definitions: {
+      MeetUp: {
+        theme_meet: 'theme_meet',
+        description_meet: 'description_meet',
+        tags: 'tags',
+        locate_meet: 'locate_meet',
+      },
+      MeetUps: [
+        {
+          $ref: '#/definitions/MeetUp'
+        }
+      ],
+      // модель объекта с текстом новой задачи
+      Text: {
+        theme_meet: 'theme_meet',
+        description_meet: 'description_meet',
+        tags: 'tags',
+        locate_meet: 'locate_meet',
+      },
+      // модель объекта с изменениями существующей задачи
+      Changes: {
+        changes: {
+            theme_meet: 'theme_meet',
+            description_meet: 'description_meet',
+            tags: 'tags',
+            locate_meet: 'locate_meet',
+        }
+      }
+    },
+    host: 'localhost:3000/api/',
+    schemes: ['http']
+   }
+
+const outputFile = join(_dirname, 'output.json')
+
+const endpointsFiles = [join(_dirname, '../routes/meetup.router.js')]
+
+swaggerAutogen()(outputFile, endpointsFiles, doc).then(({ success }) => {
+ console.log(`Generated: ${success}`)
+})
