44e041006e510eca8b5c242aaf4ecf0ba748585b
Vittalius <Vittalius@users.noreply.github.com>
2023-03-23 21:13:59 +0300

    Update index.js
    add docs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import meetUpRouter from "./routes/meetup.router.js"
+import fs from 'fs'
+import SwaggerUi from "swagger-ui-express";
+
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,7 +11,11 @@ dotenv.config();
 const app = express();
 const port = process.env.port || 8080
 
+const swaggerFile = JSON.parse(fs.readFileSync('./swagger/output.json'))
+
+
 app.use(express.json())
+app.use('/api-doc', SwaggerUi.serve, SwaggerUi.setup(swaggerFile))
 app.use('/api', meetUpRouter)
 
 app.listen(port, () => {
