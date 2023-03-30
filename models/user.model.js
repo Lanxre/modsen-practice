a05b0fab961f9d72ee197d1f6284c2c7134a37e7
Vittalius <Vittalius@users.noreply.github.com>
2023-03-30 14:02:07 +0300

    fix

--- models/user.model.js
+++ models/user.model.js
@@ -4,12 +4,14 @@ export default class User{
         id,
         username,
         password,
-        email
+        email,
+        role_id
     }){
         this.id = id;
         this.username = username;
         this.password = password;
         this.email = email;
+        this.role_id = role_id;
     }
 
 }
