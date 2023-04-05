c6ab7c9c2722ecdc98aee74cea1c2af5da956c55
Vittalius <Vittalius@users.noreply.github.com>
2023-04-05 22:25:40 +0300

    Create reg.meetup.model.js

---
+++ models/reg.meetup.model.js
@@ -0,0 +1,12 @@
+export default class RegisterMeetUp {
+
+    constructor({
+        id,
+        user_id,
+        meet_id
+    }){
+        this.id = id;
+        this.user_id = user_id;
+        this.meet_id = meet_id;
+    }
+}
